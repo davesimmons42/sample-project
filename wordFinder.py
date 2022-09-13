eaf05c3a5529e538f503c1168bde1db68a763944
davesimmons42 <86928738+davesimmons42@users.noreply.github.com>
2022-09-12 20:42:09 -0600

    Add files via upload

--- wordFinder.py
+++ wordFinder.py
@@ -11,9 +11,16 @@ known = input("Enter every character you know separated by a comma").split(",")
 sh = wrkbk.active
 
 searchable = []
+results = []
 
 for row in sh.iter_rows(min_row = 1, min_col = 1, max_row = 6000, max_col = 1):
     for cell in row:
         if(cell.value != None):
             searchable.append(cell.value)
 
+for letter in known:
+    for word in searchable:
+        if(letter in word):
+            results.append(word)
+
+print(results)
